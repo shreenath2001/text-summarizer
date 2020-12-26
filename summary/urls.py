6969944758e713eb98849904f780f413cfc2425f
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2020-12-27 00:38:33 +0530

    created base templates

--- summary/urls.py
+++ summary/urls.py
@@ -1,6 +1,10 @@
+from os import name
 from django.urls import path
 from . import views
+import summary
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('summarizer/', views.summary, name = 'summary'),
+    path('learn/', views.learn, name = 'learn'),
 ]
--- summary/views.py
+++ summary/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'summary/home.html')
+
+def summary(request):
+    return render(request, 'summary/summary.html')
+
+def learn(request):
+    return render(request, 'summary/learn.html')
