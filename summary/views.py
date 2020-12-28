cadea795e592968e0578084213e4ce0f7a026797
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2020-12-28 06:23:45 +0530

    created summarizer

--- summary/views.py
+++ summary/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from django.core.files.storage import FileSystemStorage
 import pytesseract
 from PIL import Image
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import heapq
+
+""" import spacy 
+nlp = spacy.load('en')
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest """
+
+#from spacy_textsummarizer import text_summarizer
+from nltk_textsummarizer import nltk_summarizer
 
 # Create your views here.
 def home(request):
@@ -15,8 +29,10 @@ def summary(request):
 def summarizeText(request):
     if request.method == 'POST':
         rawtext = request.POST['text']
+        summary = nltk_summarizer(rawtext)
         data = {
             'rawtext':rawtext,
+            'summary':summary,
         }
         return render(request, 'summary/result.html', data)
 
@@ -27,8 +43,10 @@ def summarizeUrl(request):
             page = urlopen(url)
             soup = BeautifulSoup(page, features = 'html.parser')
             rawtext = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+            summary = nltk_summarizer(rawtext)
             data = {
             'rawtext':rawtext,
+            'summary':summary,
         }
         except:
             problem = "Sorry, Unable to process!"
@@ -47,8 +65,10 @@ def summarizeImage(request):
         loc = '.' + imageName
         img = Image.open(loc)
         rawtext = pytesseract.image_to_string(img)
+        summary = nltk_summarizer(rawtext)
         data = {
             'rawtext':rawtext,
+            'summary':summary,
         }
         return render(request, 'summary/result.html', data)
 
