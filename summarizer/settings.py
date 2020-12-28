2cf8e76d4759eaf5c19c7cf5ec6c314cd74a717f
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2020-12-28 07:07:32 +0530

    updated database

--- summarizer/settings.py
+++ summarizer/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'summarizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+}
 
 
 # Password validation
